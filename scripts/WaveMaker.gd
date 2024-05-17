e8d3dac912b8862ffa62601176058f5428096d40
BrendanA1622 <89553639+BrendanA1622@users.noreply.github.com>
2024-05-17 08:32:21 -0700

    Starting wave mechanics
    starting wave mechanics

---
+++ scripts/WaveMaker.gd
@@ -0,0 +1,16 @@
+extends Node
+
+const RED_ENEMY = preload("res://scenes/redEnemy.tscn")
+
+@export var wave1RedEnemyNum: int
+@export var wave2RedEnemyNum: int
+@export var wave3RedEnemyNum: int
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
